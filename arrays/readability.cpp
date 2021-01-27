86edbb0e8a114e4216d91ac1a3d337fdda4b27b4
techsharif <sharif.cse.hstu@gmail.com>
2021-01-27 23:49:19 +0600

    update readability

--- arrays/readability.cpp
+++ arrays/readability.cpp
@@ -2,6 +2,7 @@
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 int main()
 {
@@ -9,7 +10,8 @@ int main()
 
     int ln = strlen(text);
 
-    int i, lcount=0, wcount=1, scount=0;
+    int i, grade;
+    float lcount=0, wcount=1, scount=0, L, S;
 
     for (i = 0; i < ln; i = i + 1){
         if (isalpha(text[i]))
@@ -28,9 +30,15 @@ int main()
         }
     }
 
-    printf("%d letter(s)\n", lcount);
-    printf("%d word(s)\n", wcount);
-    printf("%d sentence(s)\n", scount);
+    // printf("%d letter(s)\n", lcount);
+    // printf("%d word(s)\n", wcount);
+    // printf("%d sentence(s)\n", scount);
 
+    L = round((lcount * 100) / wcount);
+    S = round((scount * 100) / wcount);
+
+    grade = 0.0588 * L - 0.296 * S - 15.8;
+
+    printf("Grade %d\n", grade);
 
 }
